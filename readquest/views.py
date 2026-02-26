07405c6c1bee59a0a1fa08886f805650dabaea2d
Frida <3177108L@student.gla.ac.uk>
2026-02-26 21:25:02 +0000

    feat/ made index.hmtl, changed view

--- readquest/views.py
+++ readquest/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("testing that the view works")
+
+    return render(request,'readquest/index.html')
