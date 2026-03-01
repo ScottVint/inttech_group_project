d1c7f5f8c4e0d57bf75711ced36a6a315cb64587
Scott Vint <3143064v@student.gla.ac.uk>
2026-03-01 14:47:06 +0000

    Revert "models created"
    This reverts commit 4d45dd11612d7f62395d83bb0bab673bcdc71b21.

--- inttech_group_project/settings.py
+++ inttech_group_project/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+<<<<<<< HEAD
     'readquest',
+=======
+>>>>>>> 7562645 (Revert "models created")
 ]
 
 MIDDLEWARE = [
