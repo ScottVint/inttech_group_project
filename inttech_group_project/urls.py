f2115d5db45597b3a30fdb47503414f8251fd2cf
Frida <3177108L@student.gla.ac.uk>
2026-03-11 21:20:18 +0000

    feat: styling for mobile menu

--- inttech_group_project/urls.py
+++ inttech_group_project/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('readquest/', include('readquest.urls')),
     path('admin/', admin.site.urls),
-    path('readquest/', include('readquest.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
