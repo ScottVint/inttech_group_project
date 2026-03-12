5cee9b96cba05bd9b7fc46f88cbb4d0dfff20a34
Scott <3143064v@student.gla.ac.uk>
2026-03-12 12:21:15 +0000

    added show_details view

--- readquest/urls.py
+++ readquest/urls.py
@@ -4,8 +4,10 @@ from readquest import views
 app_name = 'readquest'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('register/', views.land_register, name='register'),
     path('login/', views.user_login, name='login'),
-    path('', views.index, name='index'),
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('details/<slug:details_slug>', views.show_details, name='show_details')
+
 ]
