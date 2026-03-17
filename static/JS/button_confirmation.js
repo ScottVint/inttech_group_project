1615686ad98260219861677d2ddcb7c41616670a
Scott Vint <3143064v@student.gla.ac.uk>
2026-03-17 17:46:13 +0000

    buttons display text correctly

--- static/JS/button_confirmation.js
+++ static/JS/button_confirmation.js
@@ -1,12 +1,11 @@
-console.log("script.js loaded, jQuery:", typeof $);
-
 $(document).ready(function () {
     
-    $(document).on("submit", ".add-book", (function(e) {
+    $(document).on("submit", ".add-book", function(e) {
     e.preventDefault();
 
     var form = $(this);
     var actionURL = form.attr('action');
+    var button = form.find(".button_two")
 
     $.ajax({
         type: 'POST',
@@ -19,11 +18,15 @@ $(document).ready(function () {
         {
             if (response.success) {
                 $("#response").text(response.message);
+                button.attr("disabled", true);
+                button.html('Added')
+                button.addClass("added")
+
             } else {
                 console.log(response.errors);
             }
         }
     });
 
-}));
+});
 });
