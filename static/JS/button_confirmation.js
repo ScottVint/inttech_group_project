37ccc54af3d076dffa9024e863731ba505a4b08d
Scott Vint <3143064v@student.gla.ac.uk>
2026-03-18 18:37:28 +0000

    Wishlist button now still clickable after set to wishlisted

--- static/JS/button_confirmation.js
+++ static/JS/button_confirmation.js
@@ -1,58 +1,32 @@
 $(document).ready(function () {
-    
-    $(".dropdown-menu").on("submit", ".add-to-reading", function(e) {
-    e.preventDefault();
+    $(".dropdown-menu").on("submit", ".add-to-reading, .add-to-wishlist", function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
 
-    var form = $(this);
-    var actionURL = form.attr('action');
-    var button = form.closest(".dropdown").find("button.button_two").first()
+        var form = $(this);
+        var isReading = form.hasClass('add-to-reading');
+        var actionURL = form.attr('action');
+        var button = form.closest(".dropdown").find("button.button_two").first();
 
-    $.ajax({
-        type: 'POST',
-        url: actionURL,
-        data: new FormData(this),
-        processData: false,
-        contentType: false,
-        success: function(response)
-        {
-            if (response.success) {
-                $("#response").text(response.message);
-                button.prop("disabled", true);
-                button.html('Reading')
-                button.addClass("added")
-
-            } else {
-                console.log(response.errors);
+        $.ajax({
+            type: 'POST',
+            url: actionURL,
+            data: new FormData(this),
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                if (response.success) {
+                    if (isReading) {
+                        button.prop("disabled", true);
+                        button.html('Reading');
+                        button.addClass("added");
+                    } else {
+                        button.html('Wishlisted').addClass("wishlisted");
+                        form.closest('li').toggle()
+                    }
+                    $("#response").text(response.message);
+                }
             }
-        }
+        });
     });
-
-});
-  $(".dropdown-menu").on("submit", ".add-to-wishlist", function(e) {
-    e.preventDefault();
-
-    var form = $(this);
-    var actionURL = form.attr('action');
-    var button = form.closest(".dropdown").find("button.button_two").first()
-
-    $.ajax({
-        type: 'POST',
-        url: actionURL,
-        data: new FormData(this),
-        processData: false,
-        contentType: false,
-        success: function(response)
-        {
-            if (response.success) {
-                $("#response").text(response.message);
-                button.html('Wishlisted')
-                button.addClass("wishlisted")
-
-            } else {
-                console.log(response.errors);
-            }
-        }
-    });
-
-});
 });
